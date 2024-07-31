28ce2b4adace7792d0c86e9ac33d8692d7363b64
Joseph  K J <josephkj47@gmail.com>
2024-07-31 21:39:58 +0000

    fix span attribute errors in home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,13 +1,16 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+from opentelemetry.context import attach, detach
 
 tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
   def run():
+
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now",now..isoformat())
+      span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -47,6 +50,5 @@ class HomeActivities:
         'replies': []
       }
       ]
-      span.set_attribute("app.result_length",len(results))
+      span.set_attribute("app.result_length", len(results))
       return results
-     )
